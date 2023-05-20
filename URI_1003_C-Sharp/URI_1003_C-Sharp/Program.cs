2fcd5dd05aebc023888ea9a25dddc2669ee5a4d4
Pedro Sangregorio <pedrohssangregorio@gmail.com>
2023-05-20 19:00:48 -0300

    URI 1003

---
+++ URI_1003_C-Sharp/URI_1003_C-Sharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uri
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            int A, B;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            int SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+
+        }
+    }
+}
