81cc1fd79c56257bb1b4eb2884d23992500882df
Pedro Sangregorio <pedrohssangregorio@gmail.com>
2023-05-20 18:15:11 -0300

    URI 1002

---
+++ URI_1002_C-Sharp/URI_1002_C-Sharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace uri
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            double A, R, π = 3.14159;
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            A = π * R * R;
+
+            Console.WriteLine("A=" + A.ToString("F4"), CultureInfo.InvariantCulture);
+        }
+    }
+}
