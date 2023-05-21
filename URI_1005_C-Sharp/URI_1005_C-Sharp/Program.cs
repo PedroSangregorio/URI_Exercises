c52f8eeeaa62311287b4d147012485aba5b5ddd6
Pedro Sangregorio <pedrohssangregorio@gmail.com>
2023-05-20 21:38:16 -0300

    URI 1005

---
+++ URI_1005_C-Sharp/URI_1005_C-Sharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace URI_1005
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            double a, b;
+
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("MEDIA = " + ((a * 3.5 + b * 7.5) / (3.5 + 7.5)).ToString("0.00000"));
+        }
+    }
+}
