a8e10833add9e47d2aa00f42da3bb865bcd659f8
Pedro Sangregorio <pedrohssangregorio@gmail.com>
2023-05-20 21:48:25 -0300

    URI 1006

---
+++ URI_1006_C-Sharp.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Globalization;
+
+namespace URI_1006
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            double a, b, c, media;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            media = (a * 2 + b * 3 + c * 5) / 10;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
